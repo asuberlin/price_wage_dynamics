c470714f7cea291cd9042869664a704ba9710fbf
gesine steudle <gesine.steudle@globalclimateforum.org>
2019-05-16 09:35:54 +0200

    branch multiHH created

--- market.py
+++ market.py
@@ -1,14 +1,14 @@
 class Market:
     def __init__(self, S_star, L_star):
-        self.SM_t = S_star
-        self.LM_t = L_star
+        self.sugar_t = S_star
+        self.labour_t = L_star
 
     #determine labor market quantity 
-    def laborTransaction(self, verbose, LD_tp1, LS_tp1):
-        self.LM_tp1 = min(LD_tp1, LS_tp1)
-        if verbose: print('Market: market labor is {:.4f}'.format(self.LM_tp1))
+    def laborTransaction(self, verbose, labourDemand, labourSupply):
+        self.labour_tp1 = min(labourDemand, labourSupply)
+        if verbose: print('Market: market labor is {:.4f}'.format(self.labour_tp1))
     
     #determine stuff market quantity
-    def stuffTransaction(self, verbose, SS_tp1, SD_tp1):
-        self.SM_tp1 = min(SS_tp1, SD_tp1)
-        if verbose: print('Market: market sugar is {:.4f}'.format(self.SM_tp1))
+    def sugarTransaction(self, verbose, sugarDemand, sugarSupply):
+        self.sugar_tp1 = min(sugarDemand, sugarSupply)
+        if verbose: print('Market: market sugar is {:.4f}'.format(self.sugar_tp1))
--- plotting.py
+++ plotting.py
@@ -10,25 +10,25 @@ def plotting(L_star, S_star, p_star, firmResults, householdResults):
     firmResults.plot(kind = 'line', x = 'step', y = 'p_t', color = 'blue', ax = ax)
     plt.axhline(y = p_star, color = 'green', ls = ':')
     plt.title('price')
-    ax.set_ylim([0, 10])
+    ax.set_ylim([0, 50])
     
     #sugar supply, demand and equilibrium
     plt.subplot(222)
     ax = plt.gca()
     firmResults.plot(kind = 'line', x = 'step', y = 'SS_t', color = 'blue', ax = ax)
-    householdResults.plot(kind = 'line', x = 'step', y = 'SD_t', color = 'red', ax = ax)
+    householdResults.plot(kind = 'line', x = 'step', y = 'sugarDemand_t', color = 'red', ax = ax)
     plt.axhline(y = S_star, color = 'green', ls = ':')
     plt.title('sugar supply and demand')
-    ax.set_ylim([0, 100])
+    ax.set_ylim([0, 200])
     
     #labor supply, demand and equilibrium
     plt.subplot(223)
     ax = plt.gca()
     firmResults.plot(kind = 'line', x = 'step', y = 'LD_t', color = 'blue', ax = ax)
-    householdResults.plot(kind = 'line', x = 'step', y = 'LS_t', color = 'red', ax = ax)
+    householdResults.plot(kind = 'line', x = 'step', y = 'labourSupply_t', color = 'red', ax = ax)
     plt.axhline(y = L_star, color = 'green', ls = ':')
     plt.title('labor supply and demand')
-    ax.set_ylim([0, 400])
+    ax.set_ylim([0, 500])
     
     plt.show()
 
@@ -43,12 +43,12 @@ def plotZandZeta(firmResults):
     ax = plt.gca()
     firmResults.plot(kind = 'line', x = 'step', y = 'z_t', color = 'blue', ax = ax)
     plt.title('z')
-    ax.set_ylim([0, 100])
+    ax.set_ylim([0, 300])
 
     plt.subplot(122)
     ax = plt.gca()
     firmResults.plot(kind = 'line', x = 'step', y = 'zeta_t', color = 'blue', ax = ax)
     plt.title('zeta')
-    ax.set_ylim([0, 1])
+    ax.set_ylim([0, 2])
     
     plt.show()
